fe3c231f45eddd775f93f2bce001ad1d551cc055
Jona <jonathanelian64@gmail.com>
2022-08-19 17:38:50 -0600

    404 page cofigured correctly

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.get('/hola-mundo', (req, res) => {
 
 app.get('*', (req, res) => {
 
-    res.sendFile(__dirname + '/public/404.html');
+    res.sendFile(__dirname + '/public/backup/404.html');
 })
 
 app.listen(port, () => {
